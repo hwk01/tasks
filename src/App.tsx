03ea62d2977f7bb54ce194abcf2398b92218beb6
hwk01 <hkline201@gmail.com>
2022-12-05 18:04:39 -0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,49 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
+            <h1> this is a header</h1>
+            <ul>
+                <li>listing things</li>
+                <li>many koalas</li>
+                <li>and kangaroos</li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                /Log Hello World/
+            </Button>
+            <img
+                src="/Users/hannah/Desktop/tasks/src/koala copy.png"
+                alt="koala"
+            />
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                Heading with React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+                Hello World <code>src/App.tsx</code> COS420
+            </p>{" "}
+            <Container>
+                <Row>
+                    <Col>
+                        {" "}
+                        <div
+                            style={ background-color: rgb(255,0,0)}
+                        >
+                            this will be surrounded by a border and padding.
+                        </div>
+                    </Col>
+                    <Col>
+                        {" "}
+                        <div
+                            style={{ border: "1px solid red", padding: "4px" }}
+                        >
+                            this will be surrounded by a border and padding.
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
