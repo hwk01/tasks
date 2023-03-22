4ae67a70b53a8f55f94584eb5a14c7f02a88fd7e
Hannah <94702589+hwk01@users.noreply.github.com>
2023-03-22 11:55:07 -0400

    Update GiveAttempts.tsx

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,41 @@
 import React, { useState } from "react";
 
 export function GiveAttempts(): JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState(3);
+    const [requestedAttempts, setRequestedAttempts] = useState("");
+
+    const handleUseClick = () => {
+        setAttemptsLeft(attemptsLeft - 1);
+    };
+
+    const handleGainClick = () => {
+        const parsedRequestedAttempts = parseInt(requestedAttempts, 10);
+        if (!isNaN(parsedRequestedAttempts)) {
+            setAttemptsLeft(attemptsLeft + parsedRequestedAttempts);
+            setRequestedAttempts("");
+        }
+    };
+
+    const handleRequestedAttemptsChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setRequestedAttempts(event.target.value);
+    };
+
+    const isUseButtonDisabled = attemptsLeft === 0;
+
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <p>Attempts left: {attemptsLeft}</p>
+            <button onClick={handleUseClick} disabled={isUseButtonDisabled}>
+                Use
+            </button>
+            <input
+                type="number"
+                value={requestedAttempts}
+                onChange={handleRequestedAttemptsChange}
+            />
+            <button onClick={handleGainClick}>Gain</button>
         </div>
     );
 }
