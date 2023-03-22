ecd954c5c4f2f8787a0fa1c16f91f6189d19f1b5
Hannah <94702589+hwk01@users.noreply.github.com>
2023-03-22 11:54:42 -0400

    Update CheckAnswer.tsx

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -5,9 +5,24 @@ export function CheckAnswer({
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState("");
+
+    const handleAnswerChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUserAnswer(event.target.value);
+    };
+
+    const answerIsCorrect =
+        userAnswer.toLowerCase() === expectedAnswer.toLowerCase();
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <input
+                type="text"
+                value={userAnswer}
+                onChange={handleAnswerChange}
+            />
+            {answerIsCorrect ? <p>✔️</p> : <p>❌</p>}
         </div>
     );
 }
