b851d80894f25287b6601ba7db24ee1125b6ba03
Hannah <94702589+hwk01@users.noreply.github.com>
2023-03-22 11:55:20 -0400

    Update MultipleChoiceQuestion.tsx

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -7,9 +7,24 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [selectedChoice, setSelectedChoice] = useState(options[0]);
+    const handleSelectChoice = (
+        event: React.ChangeEvent<HTMLSelectElement>
+    ) => {
+        setSelectedChoice(event.target.value);
+    };
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <select value={selectedChoice} onChange={handleSelectChoice}>
+                {options.map((option) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+            {selectedChoice === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
