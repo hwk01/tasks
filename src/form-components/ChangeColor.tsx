2317d54d74a308cc180ef0a109a2aeb2a94d18f9
Hannah <94702589+hwk01@users.noreply.github.com>
2023-03-22 11:54:30 -0400

    Update ChangeColor.tsx

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,47 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+const COLORS = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "violet",
+    "black"
+];
 
 export function ChangeColor(): JSX.Element {
+    const [selectedColor, setSelectedColor] = useState(COLORS[0]);
+
+    function handleColorChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setSelectedColor(e.target.value);
+    }
+
+    const colorRadios = COLORS.map((color) => (
+        <Form.Check
+            key={color}
+            inline
+            type="radio"
+            label={color}
+            name="color"
+            value={color}
+            checked={selectedColor === color}
+            onChange={handleColorChange}
+        />
+    ));
+
     return (
         <div>
             <h3>Change Color</h3>
+            <div
+                data-testid="colored-box"
+                style={{ backgroundColor: selectedColor, padding: "1rem" }}
+            >
+                {selectedColor}
+            </div>
+            <Form>{colorRadios}</Form>
         </div>
     );
 }
